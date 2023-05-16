2576d4ed487f9006cdf922289780b18df94d65b4
Parra <hpbenitez7@gmail.com>
2023-05-15 22:26:46 -0300

    arrumando index

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -5,3 +5,19 @@ sr.reveal('.area-1', {duration: 2500});
 sr.reveal('.area-2', {duration: 2500});
 
 sr.reveal('.area-3', {duration: 2500});
+
+var hamburguer = document.querySelector(".hamburguer");
+hamburguer.onclick = function(){
+  var navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("active");
+};
+
+var header = document.querySelector('header');
+
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > 0) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+});
