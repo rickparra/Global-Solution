5b3c4381a8ca40e9483fa1697ad872c8bd367a61
Parra <hpbenitez7@gmail.com>
2023-05-17 15:01:00 -0300

    pagina duvidas

---
+++ src/javascript/duvidas.js
@@ -0,0 +1,13 @@
+// Selecionar todas as perguntas
+const questions = document.querySelectorAll('.question');
+
+// Adicionar evento de clique a cada pergunta
+questions.forEach(question => {
+  question.addEventListener('click', () => {
+    // Selecionar a resposta correspondente à pergunta
+    const answer = question.nextElementSibling;
+
+    // Alternar a classe "show" para controlar a visibilidade da resposta
+    answer.classList.toggle('show');
+  });
+});
