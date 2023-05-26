b6685c98dbb33bcc00b4e1645148593f69fdf336
Parra <113686045+rickparra@users.noreply.github.com>
2023-05-25 22:16:54 -0300

    cadastrando usuario e senha

--- src/javascript/cadastro.js
+++ src/javascript/cadastro.js
@@ -9,3 +9,20 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+const loginForm = document.querySelector(".sign-in-form");
+const usernameInput = document.querySelector(".sign-in-form input[type='text']");
+const passwordInput = document.querySelector(".sign-in-form input[type='password']");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault(); // Impede o envio do formulário
+
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  if (username === "Admin" && password === "123456") {
+    window.location.href = "https://www.unicef.org/brazil/comunicados-de-imprensa/relatorio-da-onu-numeros-globais-de-fome-subiram-para-cerca-de-828-milhoes-em-2021";
+  } else {
+    alert("Usuário ou senha inválidos");
+  }
+});
