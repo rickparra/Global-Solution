81beab819a57cb3079d2d614484de304e3b7809a
Parra <113686045+rickparra@users.noreply.github.com>
2023-06-01 11:53:16 -0300

    arrumando detalhes

--- src/javascript/cadastro.js
+++ src/javascript/cadastro.js
@@ -26,3 +26,29 @@ loginForm.addEventListener("submit", (e) => {
     alert("Usuário ou senha inválidos");
   }
 });
+
+const signInForm = document.querySelector('.sign-in-form');
+const signUpForm = document.querySelector('.sign-up-form');
+
+signInForm.addEventListener('submit', function(event) {
+  const username = signInForm.querySelector('input[type="text"]').value;
+  const password = signInForm.querySelector('input[type="password"]').value;
+
+  if (!username || !password) {
+    event.preventDefault();
+    alert('Por favor, preencha todos os campos.');
+  } 
+});
+
+signUpForm.addEventListener('submit', function(event) {
+  const username = signUpForm.querySelector('input[type="text"]').value;
+  const email = signUpForm.querySelector('input[type="email"]').value;
+  const password = signUpForm.querySelector('input[type="password"]').value;
+
+  if (!username || !email || !password) {
+    event.preventDefault();
+    alert('Por favor, preencha todos os campos.');
+  } else {
+    alert('Formulário enviado com sucesso!');
+  }
+});
