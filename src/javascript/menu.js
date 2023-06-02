57f0c485db21410e1e000d79b5b52e390dcf1e0b
Parra <113686045+rickparra@users.noreply.github.com>
2023-06-01 21:33:11 -0300

    arrumando darkmode

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -1,9 +1,11 @@
+// responsividade menu
 var hamburguer = document.querySelector(".hamburguer");
 hamburguer.onclick = function(){
   var navBar = document.querySelector(".nav-bar");
   navBar.classList.toggle("active");
 };
 
+//efeito menu diminuindo e aumentando
 var header = document.querySelector('header');
 
 window.addEventListener('scroll', function() {
@@ -25,9 +27,37 @@ window.addEventListener('scroll', function() {
   }
 });
 
+//ativação dark mode
 const fixedButton = document.getElementById('fixed-button');
 const body = document.body;
 
 fixedButton.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
+  const isDarkModeEnabled = body.classList.contains('dark-mode');
+  setDarkModeCookie(isDarkModeEnabled);
+});
+
+// Função para definir o cookie com o estado do modo dark
+function setDarkModeCookie(isDarkModeEnabled) {
+  document.cookie = `darkMode=${isDarkModeEnabled}; path=/`;
+}
+
+// Função para ler o valor do cookie do modo dark
+function getDarkModeCookie() {
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith('darkMode=')) {
+      return cookie.substring('darkMode='.length) === 'true';
+    }
+  }
+  return false;
+}
+
+// Verificar o valor do cookie ao carregar a página
+window.addEventListener('DOMContentLoaded', () => {
+  const isDarkModeEnabled = getDarkModeCookie();
+  if (isDarkModeEnabled) {
+    body.classList.add('dark-mode');
+  }
 });
